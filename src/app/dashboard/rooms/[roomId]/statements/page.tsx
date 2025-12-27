53d447108e052bd0339f514792ded82636bdbe54
Ivy Jane <ivyjane033@gmail.com>
2025-12-27 17:31:25 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/dashboard/rooms/[roomId]/statements/page.tsx
+++ src/app/dashboard/rooms/[roomId]/statements/page.tsx
@@ -139,7 +139,8 @@ function ChairpersonStatementsPage() {
                     <SelectContent>
                     <SelectItem value="2025">2025</SelectItem>
                     <SelectItem value="2024">2024</SelectItem>
-                    <SelectItem value="2023">2023</SelectContent>
+                    <SelectItem value="2023">2023</SelectItem>
+                    </SelectContent>
                 </Select>
                 </div>
             </div>
